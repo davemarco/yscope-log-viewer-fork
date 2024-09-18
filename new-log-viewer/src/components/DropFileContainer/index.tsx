460edec540476721e2153d639b3f765e84d4de24
Dave Marco <marcovitch.david@gmail.com>
2024-09-18 14:36:27 +0000

    more error

--- new-log-viewer/src/components/DropFileContainer/index.tsx
+++ new-log-viewer/src/components/DropFileContainer/index.tsx
@@ -22,7 +22,7 @@ interface DropFileContextProviderProps {
  */
 const DropFileContainer = ({children}: DropFileContextProviderProps) => {
     const {loadFile} = useContext(StateContext);
-             [isFileHovering, setIsFileHovering] = useState(false);
+            const [isFileHovering, setIsFileHovering] = useState(false);
 
     const handleDrag = (ev: React.DragEvent<HTMLDivElement>) => {
         ev.preventDefault();
